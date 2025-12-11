1616aa55b406760274b219d86d77f6cad65f9324
Hung nguyen <vhung.nguyen05@gmail.com>
2025-12-11 11:26:03 +0100

    Neues Modul hinzugefügt, aber noc fehler beim Debuggen

--- Belegarbeit/Main.c
+++ Belegarbeit/Main.c
@@ -16,42 +16,68 @@ typedef struct {
 
 
 
-void datei_r(FILE* datei) {
-
-	schueler_t personen[max_schueler];
-
-
-	char erste_zeile[200];
-	datei = fopen(datei, "r");
-	fgets(erste_zeile, sizeof(erste_zeile), datei);
-
-
-	while (fscanf(datei, "%f, %29s %29s,%f ") == 4) {
-		printf(" %f, %29s %29s,%f");
-	}
-
-
-}
+//void datei_r(FILE* datei) {
+//
+//	schueler_t personen[max_schueler];
+//
+//
+//	char erste_zeile[200];
+//	datei = fopen(datei, "r");
+//	fgets(erste_zeile, sizeof(erste_zeile), datei);
+//
+//
+//	while (fscanf(datei, "%f, %29s %29s,%f ") == 4) {
+//		printf(" %f, %29s %29s,%f");
+//	}
+//
+//
+//}
 
 
 
 int main() {
 
+
+
+	schueler_t schueler[max_schueler];
+
+
+	//checks if file exist, if not it will be created
+
 	FILE* f = fopen("noten.csv", "r");
 
 	if (f == NULL) {
+		printf("Keine Datei vorhanden.\nDie Datei noten.csv wird erstellt.");
 		f = fopen("noten.csv", "w");
 		if (f != NULL) {
 			fprintf(f, "ID, Name, Vorname, Noten");
 			fclose(f);
 		}
 	}
+	
 	fclose(f);
 
 	
+
+
+
 	
+	//opens File to read  data
 	
 	f = fopen("noten.csv", "r");
+
+
+	char erste_zeile[200];
+	f = fopen(f, "r");
+	fgets(erste_zeile, sizeof(erste_zeile), f);
+
+	for (int i = 0; i < max_schueler; i++) {
+		while (fscanf(f, "%d, %29[^,] %29[^,], %d", &schueler[i].id, schueler[i].name, schueler[i].nachname, &schueler[i].note) == 4) {
+
+		}
+	}
+	fclose(f);
+
 
 
 	return 0; 
