12c0477f308c98340bb7dd9711ea51e95bf8bae6
Hung nguyen <vhung.nguyen05@gmail.com>
2025-12-13 16:13:10 +0100

    ID WIRTD 0 gesetzt aber der platzz am array ist immer noch gleich

--- Belegarbeit/loeschen.c
+++ Belegarbeit/loeschen.c
@@ -2,25 +2,36 @@
 
 
 void zeile_loeschen() {
-	
+
 	int id_auswahl;
 	char confirm;
-	
+
 	printf("Welche ID möchten sie loeschen?\nID: ");
-	scanf("%d", &id_auswahl);
+	scanf(" %d", &id_auswahl);
 	printf("Eingabe bestaetigen: [j]a/[n]ein\n");
 	scanf(" %c", &confirm);
 
-	
-		if (confirm == 'j') {
-			schueler[id_auswahl - 1].id = 0;
-			for (int i = id_auswahl+1; i < max_schueler - 1; i++) {
-				if (schueler[i].id)
+
+	if (confirm == 'j') {
+		schueler[id_auswahl - 1].id = 0;
+		for (int i = id_auswahl; i < max_schueler; i++) {
+			if (schueler[i].id != 0) {
 				schueler[i].id = schueler[i].id - 1;
 			}
+			else {
+				break;
+			}
 		}
-		else if (confirm == 'n') {
-			printf("Auswahl Nein. Zeile wurde nicht gelöscht.");
-			
-		}	
+	}
+	else if (confirm == 'n') {
+		printf("Vorgang abgebrochen.");
+	}
+	else {
+		printf("Invalid Input");
+	}
+
 }
+
+
+
+// ID WIRTD 0 gesetzt aber der platzz am array ist immer noch gleich
